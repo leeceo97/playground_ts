71a925621fda5a9f1b91c12a4eca0aca40503cc1
leeceo97 <leeceo97@naver.com>
2024-02-15 22:29:01 +0900

    finish initial settings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-console.log("Hello TypeScript");
-const a: number = 1;
-// tsc ~~ -> 타입스크립트 컴파일러가 컴파일하는 명령어 --> js로 빌드
-// node 실행 방법: node src/index.js
-// 한번에 실행시켜주는 명령어 tsx(typescript execute)
+import { hello } from "./hello";
+console.log(hello);
